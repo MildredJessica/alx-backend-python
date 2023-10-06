da7d4da8f83eabccf4bb050bb4e961fd3b00eafe
MildredJessica <nwonyejessica@gmail.com>
2023-10-06 04:57:03 +0000

    Convert a float to string and reduce the value

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import math
+
+
+def floor(n: float) -> int:
+    """
+    A function floor which takes a float n as argument
+    and returns the floor of the float..
+
+    Args:
+        n (float): The floating point number to change.
+
+    Returns:
+        float: The floor of 'n' as a int.
+    """
+    return math.floor(n)
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+def to_str(n: float) -> str:
+    """
+    Converts a float n as argument and returns the string
+    representation of the float
+
+    Args:
+    n (float): The number to be converted
+
+    Returns:
+    A string
+    """
+    return str(n)
