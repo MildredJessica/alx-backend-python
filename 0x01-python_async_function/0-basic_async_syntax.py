6892948c686969522ee59519e5c9e276f99c7608
MildredJessica <nwonyejessica@gmail.com>
2023-10-13 13:54:06 -0700

    Added return syntax

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,7 +6,7 @@ import asyncio
 from random import uniform as ru
 
 
-async def wait_random(max_delay: int = 10):
+async def wait_random(max_delay: int = 10) -> float:
     """Asynchronous coroutine that takes in an integer argument"""
     num = ru(0, max_delay)
     await asyncio.sleep(num)
