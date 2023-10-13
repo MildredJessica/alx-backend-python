3816118856c3f7324853e6484faafdd3dfb87424
MildredJessica <nwonyejessica@gmail.com>
2023-10-13 15:10:09 -0700

    Changed the return statement

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -10,6 +10,6 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 async def measure_time(n: int, max_delay: int) -> float:
     """Measures the total execution time for and returns a float"""
     start = time.perf_counter()
-    await asyncio.run(wait_n(n, max_delay))
+    asyncio.run(wait_n(n, max_delay))
     total_time = time.perf_counter() - start
     return total_time
